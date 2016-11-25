d2486fd2deb0b85e609cbe82ad8da1976267e15b
Joel Allen <joelallen720@gmail.com>
2016-11-24 22:05:38 -0800

    Setup project with default scene and simple play button.

---
+++ src/Game/Systems/Play_Button.cpp
@@ -0,0 +1,60 @@
+#include "Game/Systems/Play_Button.hpp"
+
+#include <map>
+#include "Nito/Components.hpp"
+#include "Nito/APIs/Scene.hpp"
+#include "Cpp_Utils/Map.hpp"
+
+
+using std::map;
+
+// Nito/APIs/ECS.hpp
+using Nito::Entity;
+using Nito::get_component;
+
+// Nito/Components.hpp
+using Nito::Button;
+
+// Cpp_Utils/Map.hpp
+using Cpp_Utils::remove;
+
+
+namespace Game
+{
+
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//
+// Data
+//
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+static map<Entity, Button *> entity_buttons;
+
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//
+// Interface
+//
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+void play_button_subscribe(const Entity entity)
+{
+    auto button = (Button *)get_component(entity, "button");
+    entity_buttons[entity] = button;
+
+    button->click_handler = [=]() -> void
+    {
+        puts("play button clicked!");
+    };
+}
+
+
+void play_button_unsubscribe(const Entity entity)
+{
+    static const auto dud = []() -> void {};
+
+    entity_buttons[entity]->click_handler = dud;
+    remove(entity_buttons, entity);
+}
+
+
+} // namespace Game
---
+++ src/Game/main.cpp
@@ -0,0 +1,119 @@
+#include <string>
+#include <vector>
+#include <map>
+#include "Nito/Engine.hpp"
+#include "Nito/APIs/ECS.hpp"
+#include "Nito/APIs/Input.hpp"
+#include "Nito/APIs/Window.hpp"
+#include "Cpp_Utils/Collection.hpp"
+#include "Cpp_Utils/JSON.hpp"
+
+#include "Game/Systems/Play_Button.hpp"
+
+
+using std::string;
+using std::vector;
+using std::map;
+
+// Nito/Engine.hpp
+using Nito::add_update_handler;
+using Nito::set_control_handler;
+using Nito::run_engine;
+using Nito::get_component_allocator;
+using Nito::get_component_deallocator;
+using Nito::Update_Handler;
+using Nito::Component_Handlers;
+using Nito::System_Entity_Handlers;
+
+// Nito/APIs/ECS.hpp
+using Nito::set_component_handlers;
+using Nito::set_system_entity_handlers;
+
+// Nito/APIs/Input.hpp
+using Nito::Control_Handler;
+
+// Nito/APIs/Window.hpp
+using Nito::close_window;
+
+// Cpp_Utils/Container.hpp
+using Cpp_Utils::for_each;
+
+// Cpp_Utils/JSON.hpp
+using Cpp_Utils::JSON;
+
+
+namespace Game
+{
+
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//
+// Data
+//
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+static vector<Update_Handler> game_update_handlers
+{
+
+};
+
+
+static map<string, const Control_Handler> game_control_handlers
+{
+    {
+        "exit",
+        close_window
+    },
+};
+
+
+static map<string, const System_Entity_Handlers> game_system_entity_handlers
+{
+    {
+        "play_button",
+        {
+            play_button_subscribe,
+            play_button_unsubscribe,
+        }
+    },
+};
+
+
+static map<string, const Component_Handlers> game_component_handlers
+{
+
+};
+
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//
+// Interface
+//
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+int run()
+{
+    for_each(game_update_handlers, add_update_handler);
+    for_each(game_control_handlers, set_control_handler);
+
+    for_each(
+        game_system_entity_handlers,
+        [](const string & name, const System_Entity_Handlers & system_entity_handlers) -> void
+        {
+            set_system_entity_handlers(name, system_entity_handlers.subscriber, system_entity_handlers.unsubscriber);
+        });
+
+    for_each(game_component_handlers, [](const string & type, const Component_Handlers & component_handlers) -> void
+    {
+        set_component_handlers(type, component_handlers.allocator, component_handlers.deallocator);
+    });
+
+    return run_engine();
+}
+
+
+} // namespace Game
+
+
+int main()
+{
+    return Game::run();
+}
