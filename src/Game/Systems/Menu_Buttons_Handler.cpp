b48baa899053dfeabf1e566f8cc191feadad7c7f
Joel Allen <joelallen720@gmail.com>
2017-01-10 19:03:53 -0500

    Add support for multiple subscriptions to Menu_Buttons_Handler system.

--- src/Game/Systems/Menu_Buttons_Handler.cpp
+++ src/Game/Systems/Menu_Buttons_Handler.cpp
@@ -10,6 +10,7 @@
 #include "Nito/APIs/Input.hpp"
 #include "Cpp_Utils/Collection.hpp"
 #include "Cpp_Utils/Map.hpp"
+#include "Cpp_Utils/String.hpp"
 
 #include "Game/Components.hpp"
 
@@ -51,6 +52,9 @@ using Cpp_Utils::for_each;
 using Cpp_Utils::remove;
 using Cpp_Utils::contains_key;
 
+// Cpp_Utils/String.hpp
+using Cpp_Utils::to_string;
+
 
 namespace Game
 {
@@ -58,16 +62,28 @@ namespace Game
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 //
+// Data Structures
+//
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+struct Menu_Buttons_Handler_State
+{
+    const Menu_Buttons_Handler * menu_buttons_handler;
+    string * selection_sprite_parent_id;
+    string select_handler_id;
+    map<string, Button *> buttons;
+    const Button * selected_button;
+    int selected_button_index;
+    int button_count;
+    bool dpad_reset;
+};
+
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//
 // Data
 //
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-static const string SELECT_HANDLER_ID = "menu_buttons_handler select";
-static const Menu_Buttons_Handler * entity_menu_buttons_handler;
-static string * selection_sprite_parent_id;
-static map<string, Button *> buttons;
-static const Button * selected_button;
-static int selected_button_index;
-static int button_count;
+static map<Entity, Menu_Buttons_Handler_State> entity_states;
 
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
@@ -75,18 +91,12 @@ static int button_count;
 // Utilities
 //
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-static bool entity_subscribed()
+static void select_menu_button(Menu_Buttons_Handler_State & entity_state, int index)
 {
-    return entity_menu_buttons_handler != nullptr;
-}
-
-
-static void select_menu_button(int index)
-{
-    const string & button_id = entity_menu_buttons_handler->button_ids[index];
-    selected_button_index = index;
-    selected_button = buttons.at(button_id);
-    *selection_sprite_parent_id = button_id;
+    const string & button_id = entity_state.menu_buttons_handler->button_ids[index];
+    entity_state.selected_button_index = index;
+    entity_state.selected_button = entity_state.buttons.at(button_id);
+    *entity_state.selection_sprite_parent_id = button_id;
 }
 
 
@@ -179,16 +189,20 @@ string * generate_selection_sprite()
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 void menu_buttons_handler_subscribe(Entity entity)
 {
-    if (entity_subscribed())
-    {
-        throw runtime_error("ERROR: only one entity can be subscribed to the menu_buttons_handler system per scene!");
-    }
+    static const string SELECT_HANDLER_ID_PREFIX = "menu_buttons_handler select ";
 
-    entity_menu_buttons_handler = (Menu_Buttons_Handler *)get_component(entity, "menu_buttons_handler");
-    selection_sprite_parent_id = generate_selection_sprite();
-    const vector<string> & button_ids = entity_menu_buttons_handler->button_ids;
-    const map<string, function<void()>> & button_handlers = entity_menu_buttons_handler->button_handlers;
-    button_count = button_ids.size();
+    auto menu_buttons_handler = (Menu_Buttons_Handler *)get_component(entity, "menu_buttons_handler");
+    const string select_handler_id = SELECT_HANDLER_ID_PREFIX + to_string(entity);
+    const vector<string> & button_ids = menu_buttons_handler->button_ids;
+    const int button_count = button_ids.size();
+    Menu_Buttons_Handler_State & entity_state = entity_states[entity];
+    entity_state.menu_buttons_handler = menu_buttons_handler;
+    entity_state.selection_sprite_parent_id = generate_selection_sprite();
+    entity_state.select_handler_id = select_handler_id;
+    entity_state.button_count = button_count;
+    entity_state.dpad_reset = true;
+    map<string, Button *> & buttons = entity_state.buttons;
+    const map<string, function<void()>> & button_handlers = menu_buttons_handler->button_handlers;
     const auto menu_id = (string *)get_component(entity, "id");
 
 
@@ -215,92 +229,77 @@ void menu_buttons_handler_subscribe(Entity entity)
 
 
     // Set input handlers.
-    set_controller_button_handler(SELECT_HANDLER_ID, DS4_Buttons::X, Button_Actions::PRESS, [&]() -> void
+    set_controller_button_handler(select_handler_id, DS4_Buttons::X, Button_Actions::PRESS, [&]() -> void
     {
-        selected_button->click_handler();
+        entity_state.selected_button->click_handler();
     });
 
 
     // By default, select the first button provided.
-    select_menu_button(0);
+    select_menu_button(entity_state, 0);
 }
 
 
-void menu_buttons_handler_unsubscribe(Entity /*entity*/)
+void menu_buttons_handler_unsubscribe(Entity entity)
 {
-    static const auto DUD = []() -> void {};
-
-    entity_menu_buttons_handler = nullptr;
-    selection_sprite_parent_id = nullptr;
-    selected_button = nullptr;
-
-
-    // Cleanup buttons.
-    for_each(buttons, [&](const string & /*button_id*/, Button * button) -> void
-    {
-        button->click_handler = DUD;
-    });
-
-    buttons.clear();
-
-
     // Remove input handlers.
-    remove_controller_button_handler(SELECT_HANDLER_ID);
+    remove_controller_button_handler(entity_states.at(entity).select_handler_id);
+
+
+    remove(entity_states, entity);
 }
 
 
 void menu_buttons_handler_update()
 {
-    static bool dpad_reset = true;
-
-    if (!entity_subscribed())
+    for_each(entity_states, [](Entity /*entity*/, Menu_Buttons_Handler_State & entity_state) -> void
     {
-        return;
-    }
-
-    const float d_pad_y = get_controller_axis(DS4_Axes::D_PAD_Y);
-
-
-    // When user is no longer holding d-pad down, allow for next input to be read.
-    if (!dpad_reset && d_pad_y == 0.0f)
-    {
-        dpad_reset = true;
-    }
+        const float d_pad_y = get_controller_axis(DS4_Axes::D_PAD_Y);
+        bool & dpad_reset = entity_state.dpad_reset;
+        const int selected_button_index = entity_state.selected_button_index;
 
 
-    // Ignore checking for input if user still hasn't released d-pad from last input.
-    if (!dpad_reset)
-    {
-        return;
-    }
+        // When user is no longer holding d-pad down, allow for next input to be read.
+        if (!dpad_reset && d_pad_y == 0.0f)
+        {
+            dpad_reset = true;
+        }
 
 
-    // Get new selection if user is using the d-pad.
-    int new_selected_button_index = selected_button_index;
-
-    if (d_pad_y > 0.0f)
-    {
-        new_selected_button_index++;
-    }
-    else if (d_pad_y < 0.0f)
-    {
-        new_selected_button_index--;
-    }
+        // Ignore checking for input if user still hasn't released d-pad from last input.
+        if (!dpad_reset)
+        {
+            return;
+        }
 
 
-    // Ensure the new selected index is different than the current selected index, and is within range of the number of
-    // buttons in the menu before switching the selected button.
-    if (new_selected_button_index != selected_button_index &&
-        new_selected_button_index >= 0 &&
-        new_selected_button_index < button_count)
-    {
-        select_menu_button(new_selected_button_index);
+        // Get new selection if user is using the d-pad.
+        int new_selected_button_index = selected_button_index;
+
+        if (d_pad_y > 0.0f)
+        {
+            new_selected_button_index++;
+        }
+        else if (d_pad_y < 0.0f)
+        {
+            new_selected_button_index--;
+        }
 
 
-        // Now that user input has been read to change the menu selection, wait until user releases d-pad to read next
-        // input, preventing changing selection once per frame for every frame the d-pad is held down.
-        dpad_reset = false;
-    }
+        // Ensure the new selected index is different than the current selected index, and is within range of the number of
+        // buttons in the menu before switching the selected button.
+        if (new_selected_button_index != selected_button_index &&
+            new_selected_button_index >= 0 &&
+            new_selected_button_index < entity_state.button_count)
+        {
+            select_menu_button(entity_state, new_selected_button_index);
+
+
+            // Now that user input has been read to change the menu selection, wait until user releases d-pad to read next
+            // input, preventing changing selection once per frame for every frame the d-pad is held down.
+            dpad_reset = false;
+        }
+    });
 }
 
 
