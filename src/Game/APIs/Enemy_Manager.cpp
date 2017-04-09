7800178224dab355a45dc5d1ebaa30dd37f815c1
Joel Allen <joelallen720@gmail.com>
2017-04-09 13:59:51 -0700

    Add basic boss generation to Enemy_Manager API. Update boss blueprint. Add bosses.json data file for storing boss data.

--- src/Game/APIs/Enemy_Manager.cpp
+++ src/Game/APIs/Enemy_Manager.cpp
@@ -66,6 +66,32 @@ static const string ROOM_CHANGE_HANDLER_ID("enemy_manager");
 static Enemies * enemies;
 static const vec3 * room_tile_texture_scale;
 static vector<vector<JSON>> enemy_layouts;
+static map<string, const JSON> boss_datas;
+
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//
+// Utilities
+//
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+static void generate_enemy(const string & blueprint, const vec3 & position, const int room)
+{
+    const Entity enemy_entity = load_blueprint(blueprint);
+    ((Transform *)get_component(enemy_entity, "transform"))->position = position;
+
+    ((Health *)get_component(enemy_entity, "health"))->death_handlers["enemy_manager"] = [=]() -> void
+    {
+        // Remove enemy from its associated room's enemy count.
+        remove_enemy(room);
+
+        game_manager_untrack_render_flag(room, enemy_entity);
+        game_manager_untrack_collider_enabled_flag(room, enemy_entity);
+    };
+
+    add_enemy(room);
+    game_manager_track_render_flag(room, enemy_entity);
+    game_manager_track_collider_enabled_flag(room, enemy_entity);
+}
 
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
@@ -76,6 +102,7 @@ static vector<vector<JSON>> enemy_layouts;
 void enemy_manager_api_init()
 {
     enemy_layouts = read_json_file("resources/data/enemy_layouts.json").get<vector<vector<JSON>>>();
+    boss_datas = read_json_file("resources/data/bosses.json").get<map<string, const JSON>>();
 }
 
 
@@ -86,9 +113,11 @@ void generate_enemies()
     const int room_tile_height = get_room_tile_height();
     const int enemies_width = floor_size * room_tile_width;
     const int enemies_height = floor_size * room_tile_height;
-    const int max_room_id = get_max_room_id();
+    const int boss_room = get_max_room_id();
     enemies = new Enemies[enemies_width * enemies_height];
     room_tile_texture_scale = &get_room_tile_texture_scale();
+    int boss_room_x = 0;
+    int boss_room_y = 0;
 
 
     // Generate enemy data.
@@ -99,9 +128,18 @@ void generate_enemies()
 
     iterate_rooms([&](int x, int y, int & room) -> void
     {
-        // Don't generate enemies for non-rooms, the spawn room, or the boss room.
-        if (room == 0 || room == 1 || room == max_room_id)
+        // Don't generate enemies for non-rooms or spawn room.
+        if (room == 0 || room == 1)
         {
+            return;
+        }
+
+
+        // Don't generate enemies for boss room, but store its coordinates for boss generation.
+        if (room == boss_room)
+        {
+            boss_room_x = x;
+            boss_room_y = y;
             return;
         }
 
@@ -136,24 +174,30 @@ void generate_enemies()
             return;
         }
 
-        const Entity enemy_entity = load_blueprint(ENEMY_BLUEPRINTS.at(enemy));
         const vec3 enemy_position = vec3(x, y, 0) * *room_tile_texture_scale;
-        const int enemy_room = get_room(enemy_position);
-        ((Transform *)get_component(enemy_entity, "transform"))->position = enemy_position;
-
-        ((Health *)get_component(enemy_entity, "health"))->death_handlers["enemy_manager"] = [=]() -> void
-        {
-            // Remove enemy from its associated room's enemy count.
-            remove_enemy(enemy_room);
-
-            game_manager_untrack_render_flag(enemy_room, enemy_entity);
-            game_manager_untrack_collider_enabled_flag(enemy_room, enemy_entity);
-        };
-
-        add_enemy(enemy_room);
-        game_manager_track_render_flag(enemy_room, enemy_entity);
-        game_manager_track_collider_enabled_flag(enemy_room, enemy_entity);
+        generate_enemy(ENEMY_BLUEPRINTS.at(enemy), enemy_position, get_room(enemy_position));
     });
+
+
+    // Generate boss.
+    static const vector<string> BOSS_IDS
+    {
+        "boss",
+    };
+
+    const string & boss_id = BOSS_IDS[random(0, BOSS_IDS.size())];
+    const JSON & boss_spawn_position = boss_datas.at(boss_id)["spawn_position"];
+    const int boss_room_origin_x = boss_room_x * room_tile_width;
+    const int boss_room_origin_y = boss_room_y * room_tile_height;
+
+    const vec3 boss_position =
+        vec3(
+            boss_room_origin_x + boss_spawn_position["x"].get<int>(),
+            boss_room_origin_y + boss_spawn_position["y"].get<int>(),
+            0) *
+        *room_tile_texture_scale;
+
+    generate_enemy(boss_id, boss_position, boss_room);
 }
 
 
